4877f0e749db7fa622abc3817354b50a365fcda4
d0F4M1N <127757642+d0F4M1N@users.noreply.github.com>
2023-11-27 01:22:16 +0300

    Update Source.cpp
    Update Aboba5
    Теперь вычисления делаются по формулам из методички

--- ABOBA5/ABOBA5/Source.cpp
+++ ABOBA5/ABOBA5/Source.cpp
@@ -1,73 +1,91 @@
 #include <iostream>
 #include <cmath>
 
-// Подынтегральная функция
+// РџРѕРґС‹РЅС‚РµРіСЂР°Р»СЊРЅР°СЏ С„СѓРЅРєС†РёСЏ
 double f(double x) {
     return std::sqrt(x + std::pow(x, 3));
 }
 
-// Метод трапеций с критерием завершения
+// РњРµС‚РѕРґ С‚СЂР°РїРµС†РёР№ СЃ РєСЂРёС‚РµСЂРёРµРј Р·Р°РІРµСЂС€РµРЅРёСЏ
 double trapezoidalIntegration(double a, double b, double epsilon) {
     int n = 1;
     double h = b - a;
-    double integral_prev, integral_current = 0;
+    double integral_h, integral_h_half;
 
     do {
-        integral_prev = integral_current;
         h /= 2.0;
-        double sum = 0.0;
 
-        for (int i = 0; i < n; ++i) {
-            sum += f(a + (2 * i + 1) * h);
+        // Р’С‹С‡РёСЃР»РµРЅРёРµ РёРЅС‚РµРіСЂР°Р»Р° РґР»СЏ h
+        integral_h = 0.5 * (f(a) + f(b));
+        for (int i = 1; i < n; ++i) {
+            integral_h += f(a + i * h);
         }
+        integral_h *= h;
 
-        integral_current = 0.5 * integral_prev + h * sum;
+        // Р’С‹С‡РёСЃР»РµРЅРёРµ РёРЅС‚РµРіСЂР°Р»Р° РґР»СЏ h / 2
+        integral_h_half = 0.5 * (f(a) + f(b));
+        for (int i = 1; i < 2 * n; ++i) {
+            integral_h_half += f(a + 0.5 * i * h);
+        }
+        integral_h_half *= 0.5 * h;
+
         n *= 2;
 
-    } while (std::abs(integral_current - integral_prev) > epsilon);
+    } while (std::abs(integral_h_half - integral_h) > 3 * epsilon);
 
-    return integral_current;
+    return integral_h_half;
 }
 
-// Метод Симпсона с критерием завершения
+// РњРµС‚РѕРґ РЎРёРјРїСЃРѕРЅР° СЃ РєСЂРёС‚РµСЂРёРµРј Р·Р°РІРµСЂС€РµРЅРёСЏ
 double simpsonIntegration(double a, double b, double epsilon) {
     int n = 1;
     double h = b - a;
-    double integral_prev, integral_current = 0;
+    double integral_h, integral_h_half;
 
     do {
-        integral_prev = integral_current;
         h /= 2.0;
-        double sum = 0.0;
 
-        for (int i = 0; i < n; ++i) {
-            sum += (i % 2 == 0) ? 2 * f(a + i * h) : 4 * f(a + i * h);
+        // Р’С‹С‡РёСЃР»РµРЅРёРµ РёРЅС‚РµРіСЂР°Р»Р° РґР»СЏ h
+        integral_h = f(a) + f(b);
+
+        for (int i = 1; i < n; ++i) {
+            integral_h += (i % 2 == 0) ? 2 * f(a + i * h) : 4 * f(a + i * h);
         }
 
-        integral_current = (h / 3.0) * (f(a) + sum + f(b));
+        integral_h = (h / 3.0) * integral_h;
+
+        // Р’С‹С‡РёСЃР»РµРЅРёРµ РёРЅС‚РµРіСЂР°Р»Р° РґР»СЏ h / 2
+        integral_h_half = f(a) + f(b);
+
+        for (int i = 1; i < 2 * n; ++i) {
+            integral_h_half += (i % 2 == 0) ? 2 * f(a + 0.5 * i * h) : 4 * f(a + 0.5 * i * h);
+        }
+
+        integral_h_half = (0.5 * h / 3.0) * integral_h_half;
+
         n *= 2;
 
-    } while (std::abs(integral_current - integral_prev) > epsilon);
+    } while (std::abs(integral_h_half - integral_h) > 15 * epsilon);
 
-    return integral_current;
+    return integral_h_half;
 }
 
 int main() {
-    // Интервал
+    // РРЅС‚РµСЂРІР°Р»
     double a = 0.6, b = 1.724;
 
-    // Заданные значения для epsilon
+    // Р—Р°РґР°РЅРЅС‹Рµ Р·РЅР°С‡РµРЅРёСЏ РґР»СЏ epsilon
     double epsilon1 = 1e-4;
     double epsilon2 = 1e-5;
 
-    // Решение задачи методом трапеций с критерием завершения
+    // Р РµС€РµРЅРёРµ Р·Р°РґР°С‡Рё РјРµС‚РѕРґРѕРј С‚СЂР°РїРµС†РёР№ СЃ РєСЂРёС‚РµСЂРёРµРј Р·Р°РІРµСЂС€РµРЅРёСЏ
     double trap_result1 = trapezoidalIntegration(a, b, epsilon1);
     double trap_result2 = trapezoidalIntegration(a, b, epsilon2);
 
     std::cout << "Trapezoidal Integration Result (epsilon = 1e-4): " << trap_result1 << std::endl;
     std::cout << "Trapezoidal Integration Result (epsilon = 1e-5): " << trap_result2 << std::endl;
 
-    // Решение задачи методом Симпсона с критерием завершения
+    // Р РµС€РµРЅРёРµ Р·Р°РґР°С‡Рё РјРµС‚РѕРґРѕРј РЎРёРјРїСЃРѕРЅР° СЃ РєСЂРёС‚РµСЂРёРµРј Р·Р°РІРµСЂС€РµРЅРёСЏ
     double simpson_result1 = simpsonIntegration(a, b, epsilon1);
     double simpson_result2 = simpsonIntegration(a, b, epsilon2);
 
